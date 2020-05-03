70007e036c05bd1261c4e01fbad23f870ffafee9
alexemm <al_emmerich@gmx.de>
2020-05-04 00:22:09 +0200

    Changed marker algorithm into returning set of variables

--- horn_sat.py
+++ horn_sat.py
@@ -1,21 +1,21 @@
-from cnf_formula import HornFormulaSet
+from typing import Set, Optional
 
-# TODO: Change the set of literals into set of variables (literals are not needed in return), also add return type
+from cnf_formula import HornFormulaSet, Variable
 
 
-def marker_algorithm(psi: HornFormulaSet):
+def marker_algorithm(psi: HornFormulaSet) -> Optional[Set[Variable]]:
     """
     Applies Marker Algorithm to given Horn Formula and returns either minimal model (if satisfiable) or None
     :param psi: Horn Formula represented as HornFormulaSet
     :return: Minimal model as set of Variables or None
     """
-    N = set()
-    M = set([X.literals.copy().pop() for X in psi.get_right_side_given()])
+    N: Set[Variable] = set()
+    M: Set[Variable] = set([X.literals.copy().pop().variable for X in psi.get_right_side_given()])
     while N != M:
-        N = M
-        M = M | set(
-            [X.get_positive_literals().copy().pop() for X in psi.get_one_positve_many_negative() if
-             set(map(lambda x: -x, X.get_negative_literals())) <= M])
-        if any([set(map(lambda x: -x, clause.get_negative_literals())) <= M for clause in psi.get_just_negative()]):
+        N: Set[Variable] = M
+        M: Set[Variable] = M | set(
+            [X.get_positive_literals().copy().pop().variable for X in psi.get_one_positve_many_negative() if
+             set([x.variable for x in X.get_negative_literals()]) <= M])
+        if any([set([x.variable for x in clause.get_negative_literals()]) <= M for clause in psi.get_just_negative()]):
             return None
     return M
