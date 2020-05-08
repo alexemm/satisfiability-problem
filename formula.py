bbfa632e21d404e3fb2b35087dbb9b4d7bbd82b4
alexemm <al_emmerich@gmx.de>
2020-05-08 15:44:54 +0200

    Added type hints in abstract classes

--- formula.py
+++ formula.py
@@ -7,18 +7,18 @@ class Variable:
     set.
     """
 
-    def __init__(self, name: str):
-        self.name = name
+    def __init__(self, name: str) -> None:
+        self.name: str = name
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Variable') -> bool:
         if not isinstance(other, type(self)):
             return NotImplemented
         return self.name == other.name
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self.name)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -28,25 +28,25 @@ class Literal:
     positive or negative.
     """
 
-    def __init__(self, variable: Variable, postive: bool):
-        self.variable = variable
-        self.positive = postive
+    def __init__(self, variable: Variable, postive: bool) -> None:
+        self.variable: Variable = variable
+        self.positive: bool = postive
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Literal') -> bool:
         if not isinstance(other, type(self)):
             return NotImplemented
         return self.variable == other.variable and self.positive == other.positive
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(str(self.variable) + str(self.positive))
 
-    def __str__(self):
-        pre = ""
+    def __str__(self) -> str:
+        pre: str = ""
         if not self.positive:
-            pre = "¬"
+            pre: str = "¬"
         return "%s%s" % (pre, str(self.variable))
 
-    def __neg__(self):
+    def __neg__(self) -> 'Literal':
         return Literal(self.variable, not self.positive)
 
 
@@ -55,21 +55,21 @@ class Clause:
     Abstract class Clause which is a set of literals
     """
 
-    def __init__(self, literals: Set[Literal]):
-        self.literals = literals
+    def __init__(self, literals: Set[Literal]) -> None:
+        self.literals: Set[Literal] = literals
 
-    def __eq__(self, other):
+    def __eq__(self, other: 'Clause') -> bool:
         if not isinstance(other, type(self)):
             return NotImplemented
         return self.literals == other.literals
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(frozenset(self.literals))
 
-    def __str__(self):
+    def __str__(self) -> str:
         return '{' + ','.join([str(literal) for literal in self.literals]) + '}'
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.literals)
 
 
@@ -78,11 +78,11 @@ class ClauseSet:
     Class which represents sets of clauses
     """
 
-    def __init__(self, clause_set: Set[Clause]):
-        self.clause_set = clause_set
+    def __init__(self, clause_set: Set[Clause]) -> None:
+        self.clause_set: Set[Clause] = clause_set
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.clause_set)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return '{' + ','.join((map(str, self.clause_set))) + '}'
